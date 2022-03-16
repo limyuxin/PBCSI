4b3d320fe12c76f600038f50d80652cfb9ac8442
leezhenglong-dev <84690187+leezhenglong-dev@users.noreply.github.com>
2022-03-16 11:04:08 +0800

    modify test_program

--- Lecture_2/test_program.cpp
+++ Lecture_2/test_program.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main()
 {
+	int x = 123;  //integer 
+	char y = 'z'; //character
+	string z = "apple orange watermelon";  //word, sentence
+	
+	//output
 	cout<<"Welcome to PBCSI"<<endl;
+	cout<<"Hello world!"<<endl;
+	cout<<x<<endl;
+	cout<<y<<endl;
+	cout<<z<<endl;
+	
+	//console output -> cout
 	
 	return 0;
 }
